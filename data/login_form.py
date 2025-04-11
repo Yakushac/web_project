98a73943eb7bae5c9a9e8bbcddb0f88903d08565
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-04-11 21:31:42 +0400

    Update login_form.py

--- data/login_form.py
+++ data/login_form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
+from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 
 
@@ -17,3 +18,15 @@ class RegisterForm(FlaskForm):
     name = StringField('Имя пользователя', validators=[DataRequired()])
     about = TextAreaField("Немного о себе")
     submit = SubmitField('Войти')
+
+
+class AddProduct(FlaskForm):
+    title = StringField('Название продукта', validators=[DataRequired()])
+    content = TextAreaField("Описание", validators=[DataRequired()])
+    category = SelectField("Категория", choices=['Футболки', 'Свитшоты', 'Толстовки', 'Брюки', 'Джинсы',
+                                                 'Кроссовки', 'Туфли', 'Головные уборы', 'Бижутерия', 'Общее'],
+                           validators=[DataRequired()])
+    is_private = BooleanField('Сделать продукт приватным:')
+    price = StringField('Цена', default='0')
+    image = StringField('Ссылка на изображение:', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
