ea052ca4136a3c1925ad46ded4117206dce1bc04
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-04-11 21:32:25 +0400

    Update products.py

--- data/products.py
+++ data/products.py
@@ -9,13 +9,15 @@ class Products(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    # price = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    price = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
+    category = sqlalchemy.Column(sqlalchemy.String, default='Общее')
+    image = sqlalchemy.Column(sqlalchemy.String)
     user = orm.relationship('User')
