f4a69bda7238cafb7ab9339241823fa5a2346ccf
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-04-11 21:22:45 +0400

    Add files via upload

---
+++ data/liked_products.py
@@ -0,0 +1,21 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Liked(SqlAlchemyBase):
+    __tablename__ = 'liked'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    category = sqlalchemy.Column(sqlalchemy.String, default='Общее')
+    image = sqlalchemy.Column(sqlalchemy.String)
+    user = orm.relationship('User')
