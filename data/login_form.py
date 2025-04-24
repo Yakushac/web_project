095934bdfd5521f76f09026f7e7d220cc4d31f52
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-04-24 15:16:48 +0400

    Update login_form.py

--- data/login_form.py
+++ data/login_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField, TimeField
 from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 
@@ -30,3 +30,12 @@ class AddProduct(FlaskForm):
     price = StringField('Цена', default='0')
     image = StringField('Ссылка на изображение:', validators=[DataRequired()])
     submit = SubmitField('Добавить')
+
+
+class MakeAnOrder(FlaskForm):
+    address = StringField('Адрес доставки', validators=[DataRequired()])
+    size = SelectField('Размер', choices=['XXS', 'XS', 'S', 'M', 'L', 'XL'])
+    time = TimeField('Время доставки')
+    is_delivery_paid = BooleanField('Экспресс-доставка: ')
+    payment_method = SelectField('Способ оплаты', choices=['Наличными', 'По карте'])
+    submit = SubmitField('Оформить заказ')
