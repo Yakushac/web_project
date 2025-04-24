3e75f568ef918357389a051f4b1f4c1d077341f9
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-04-24 15:02:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from data.db_session import create_session
 from data.users import User
 from data.products import Products
 from data.liked_products import Liked
+from data.ordered_products import Ordered
 import datetime
-from data.login_form import LoginForm, RegisterForm, AddProduct
+from data.login_form import LoginForm, RegisterForm, AddProduct, MakeAnOrder
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
@@ -23,6 +24,27 @@ def main():
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
+
+
+@app.route('/tshirts')
+def tshirts():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Футболки')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/sweatshirts')
+def sweatshirts():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Свитшоты')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/hoodies')
+def hoodies():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Толстовки')
+    return render_template("main_page.html", all_products=products)
 
 
 @app.route("/cookie_test")
@@ -101,7 +123,44 @@ def main_page():
 
 @app.route('/trousers')
 def trousers():
-    return '''fsfds'''
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Брюки')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/jeans')
+def jeans():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Джинсы')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/sneakers')
+def sneakers():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Кроссовки')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/shoes')
+def shoes():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Туфли')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/headdress')
+def headdress():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Головные уборы')
+    return render_template("main_page.html", all_products=products)
+
+
+@app.route('/jewelry')
+def jewerly():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Бижутерия')
+    return render_template("main_page.html", all_products=products)
 
 
 @app.route('/add_product', methods=['GET', 'POST'])
@@ -149,6 +208,71 @@ def add_basket():
             return redirect('/')
         except:
             return redirect('/')
+
+
+@app.route('/make_an_order/<int:id>', methods=['GET', 'POST'])
+@login_required
+def make_an_order(id):
+    db_sess = db_session.create_session()
+    form = MakeAnOrder()
+    # db_sess = db_session.create_session()
+    # order = make_an_order_1()
+    # if form2.validate_on_submit():
+    #     ordered = Ordered(
+    #         id=order[0],
+    #         title=order[1],
+    #         content=order[2],
+    #         price=order[3],
+    #         image=order[4],
+    #         category=order[5],
+    #         address=form2.address.data,
+    #         time=form2.time.data,
+    #         is_delivery_paid=form2.is_delivery_paid.data,
+    #         payment_method=form2.payment_method.data,
+    #         size=form2.size.data,
+    #         user_id=request.form.get('user_id')
+    #     )
+    #     try:
+    #         db_sess.add(ordered)
+    #         db_sess.commit()
+    #         return redirect('/ordered_products')
+    #     except:
+    #         return redirect('/')
+    # return render_template('make_an_order.html', title='Добавьте новый продукт', form=form2)
+
+    if request.method == 'POST':
+        if current_user.is_authenticated:
+            product = db_sess.query(Products).filter(Products.id == id).first()
+            address = form.address.data
+            time = form.time.data
+            size = form.size.data
+            is_delivery_paid = form.is_delivery_paid.data
+            payment_method = form.payment_method.data
+
+            ordered_item = Ordered(
+                title=product.title,
+                price=product.price,
+                content=product.content,
+                is_private=product.is_private,
+                user_id=current_user.id,
+                category=product.category,
+                image=product.image,
+                address=address,
+                time=time,
+                size=size,
+                is_delivery_paid=is_delivery_paid,
+                payment_method=payment_method
+            )
+
+            db_sess.add(ordered_item)
+            db_sess.commit()
+            return redirect('/ordered_products')
+    return render_template('make_an_order.html', form=form)
+
+
+@app.route('/ordered_products')
+def ordered_products():
+    return """fdfsfdfsfsd"""
 
 
 @app.route('/logout')
