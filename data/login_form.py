90fa7828f0ae20626dee23c4f5f2369f3ae04784
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-05-10 16:31:27 +0400

    Update login_form.py

--- data/login_form.py
+++ data/login_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField, TimeField
 from wtforms.fields.choices import SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class LoginForm(FlaskForm):
@@ -48,3 +48,43 @@ class MakeAnOrder(FlaskForm):
     is_delivery_paid = BooleanField('Экспресс-доставка: ')
     payment_method = SelectField('Способ оплаты', choices=['Наличными', 'По карте'])
     submit = SubmitField('Оформить заказ')
+
+
+class AddCard(FlaskForm):
+    number = StringField('Номер',
+                         validators=[DataRequired(), Length(min=16, max=16, message=None)])
+
+    def validate_number(form, field):
+        if not field.data.isdigit():
+            raise ValidationError('Введите корректный номер карты')
+
+    expiration_date = StringField('Срок действия', validators=[DataRequired(), Length(min=4, max=4,
+                                                                                      message=None)])
+
+    def validate_expiration_date(form, field):
+        if not field.data.isdigit():
+            raise ValidationError("Введите корректный срок действия карты")
+
+    cvc = StringField('CVC', validators=[DataRequired(), Length(min=3, max=3,
+                                                                message=None)])
+
+    def validate_cvc(form, field):
+        if not field.data.isdigit():
+            raise ValidationError("Введите корректный CVC карты")
+
+    submit = SubmitField('Подтвердить')
+
+
+class Balance(FlaskForm):
+    balance = StringField('Укажите сумму списания:', validators=[DataRequired()])
+
+    def validate_balance(form, field):
+        if not field.data.isdigit():
+            raise ValidationError('Введите корректную сумму')
+
+    submit = SubmitField('Подтвердить')
+
+
+class MakeReview(FlaskForm):
+    review = TextAreaField('Напишите отзыв о данном продукте: ', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')
