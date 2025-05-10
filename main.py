c6b4b71d480db28564dbb360eff0d8a7f28cb8d5
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-05-10 16:43:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from data.users import User
 from data.products import Products
 from data.liked_products import Liked
 from data.ordered_products import Ordered
-import datetime
-from data.login_form import LoginForm, RegisterForm, AddProduct, MakeAnOrder, SearchFilter
+from data.cards import Cards
+from data.reviews import Reviews
+from data.login_form import LoginForm, RegisterForm, AddProduct, MakeAnOrder, SearchFilter, AddCard, Balance, MakeReview
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask import abort
 
@@ -31,7 +32,8 @@ def load_user(user_id):
 def tshirts():
     db_sess = db_session.create_session()
     products = db_sess.query(Products).filter(Products.is_private != True, Products.category == 'Футболки')
-    return render_template("main_page.html", all_products=products)
+    if current_user.is_authenticated:
+        return render_template("main_page.html", all_products=products)
 
 
 @app.route('/sweatshirts')
@@ -80,7 +82,7 @@ def login():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -105,9 +107,10 @@ def reqister():
 
 
 @app.route('/home')
+@login_required
 def home():
     db_sess = db_session.create_session()
-    products = db_sess.query(Products).filter(Products.user_id == current_user.id)
+    products = db_sess.query(Products).filter(Products.user_id == current_user.id).all()
     if current_user.is_authenticated:
         return render_template('home.html', name=current_user.name, email=current_user.email,
                                about=current_user.about, products=products)
@@ -165,6 +168,7 @@ def jewerly():
 
 
 @app.route('/add_product', methods=['GET', 'POST'])
+@login_required
 def add_product():
     form = AddProduct()
     if form.validate_on_submit():
@@ -184,39 +188,54 @@ def add_product():
 
 
 @app.route('/basket')
+@login_required
 def basket():
     db_sess = db_session.create_session()
-    liked = db_sess.query(Liked).filter(Liked.user_id == current_user.id)
+    liked = db_sess.query(Liked).filter(Liked.user_id == current_user.id).all()
     return render_template('basket.html', liked=liked)
 
 
-@app.route('/add_basket', methods=['GET', 'POST'])
-def add_basket():
-    if request.method == 'POST':
-        if current_user.is_authenticated:
-            db_sess = db_session.create_session()
-            is_have = db_sess.query(Liked).filter(Liked.product_id == request.form.get('id'),
-                                                  Liked.user_id == current_user.id).all()
-            if not is_have:
-                new_liked = Liked(
-                    product_id=request.form.get('id'),
-                    title=request.form.get('title'),
-                    user_id=request.form.get('user_id'),
-                    content=request.form.get('content'),
-                    price=request.form.get('price'),
-                    image=request.form.get('image'),
-                    category=request.form.get('category'),
-                )
-                try:
-                    db_sess.add(new_liked)
-                    db_sess.commit()
-                    return redirect('/')
-                except:
-                    return redirect('/')
-            else:
-                return redirect('/notation')
+@app.route('/add_basket/<int:id>', methods=['GET', 'POST'])
+@login_required
+def add_basket(id):
+    if current_user.is_authenticated:
+        db_sess = db_session.create_session()
+        is_have = db_sess.query(Liked).filter(Liked.product_id == id,
+                                              Liked.user_id == current_user.id).first()
+        product = db_sess.query(Products).filter(Products.id == id).first()
+        if not is_have:
+            new_liked = Liked(
+                product_id=id,
+                title=product.title,
+                user_id=current_user.id,
+                content=product.content,
+                price=product.price,
+                image=product.image,
+                category=product.category,
+            )
+            try:
+                db_sess.add(new_liked)
+                db_sess.commit()
+                return redirect('/')
+            except:
+                return redirect('/')
         else:
             return redirect('/notation')
+    else:
+        return redirect('/notation')
+
+
+@app.route('/delete_basket/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_basket(id):
+    db_sess = db_session.create_session()
+    product = db_sess.query(Liked).filter(Liked.id == id, Liked.user_id == current_user.id).first()
+    if product:
+        db_sess.delete(product)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/basket')
 
 
 @app.route('/notation')
@@ -231,43 +250,58 @@ def make_an_order(id):
     form = MakeAnOrder()
 
     if request.method == 'POST':
-        if current_user.is_authenticated:
+        if form.validate_on_submit():
             product = db_sess.query(Products).filter(Products.id == id).first()
-            address = form.address.data
-            time = form.time.data
-            size = form.size.data
-            is_delivery_paid = form.is_delivery_paid.data
-            payment_method = form.payment_method.data
+            user = db_sess.query(User).filter(User.id == current_user.id).first()
+            if current_user.balance >= product.price:
+                user.balance = user.balance - product.price
+                address = form.address.data
+                time = form.time.data
+                size = form.size.data
+                is_delivery_paid = form.is_delivery_paid.data
+                payment_method = form.payment_method.data
 
-            ordered_item = Ordered(
-                product_id=product.id,
-                title=product.title,
-                price=product.price,
-                content=product.content,
-                is_private=product.is_private,
-                user_id=current_user.id,
-                category=product.category,
-                image=product.image,
-                address=address,
-                time=time,
-                size=size,
-                is_delivery_paid=is_delivery_paid,
-                payment_method=payment_method,
-                created_date=product.created_date
-            )
+                ordered_item = Ordered(
+                    product_id=product.id,
+                    title=product.title,
+                    price=product.price,
+                    content=product.content,
+                    is_private=product.is_private,
+                    user_id=current_user.id,
+                    category=product.category,
+                    image=product.image,
+                    address=address,
+                    time=time,
+                    size=size,
+                    is_delivery_paid=is_delivery_paid,
+                    payment_method=payment_method,
+                    created_date=product.created_date
+                )
 
-            db_sess.add(ordered_item)
-            db_sess.commit()
-            return redirect('/ordered_products')
+                db_sess.add(ordered_item)
+                db_sess.commit()
+                return redirect('/ordered_products')
+            else:
+                return '''Недостаточно средств. Пополните баланс и сделайте заказ заново.'''
     return render_template('make_an_order.html', form=form)
 
 
-@app.route('/cancel_order/<int:id>', methods=['GET', 'POST'])
+@app.route('/reviews/<int:id>')
+def reviews(id):
+    db_sess = db_session.create_session()
+    reviews = db_sess.query(Reviews).filter(Reviews.product_id == id).all()
+    product = db_sess.query(Products).filter(Products.id == id).first()
+    return render_template('view_reviews.html', reviews=reviews, product=product)
+
+
+@app.route('/cancel_order/<int:id>')
 @login_required
 def cancel_order(id):
     db_sess = db_session.create_session()
     products = db_sess.query(Ordered).filter(Ordered.id == id, Ordered.user_id == current_user.id).first()
+    user = db_sess.query(User).filter(User.id == current_user.id).first()
     if products:
+        user.balance = user.balance + products.price
         db_sess.delete(products)
         db_sess.commit()
     else:
@@ -326,11 +360,11 @@ def edit_product(id):
             abort(404)
     return render_template('add_product.html',
                            title='Изменить сведения о продукте',
-                           form=form
-                           )
+                           form=form)
 
 
 @app.route('/ordered_products')
+@login_required
 def ordered_products():
     db_sess = db_session.create_session()
     ordered = db_sess.query(Ordered).filter(Ordered.user_id == current_user.id)
@@ -360,12 +394,122 @@ def filtered(min_and_max):
         category = min_and_max.split(' ')[2]
         db_sess = db_session.create_session()
         filtered_products = db_sess.query(Products).filter(Products.price >= min_price,
-                                                           Products.price <= max_price, Products.category == category).all()
+                                                           Products.price <= max_price,
+                                                           Products.category == category).all()
         return render_template('main_page.html', all_products=filtered_products)
     except:
         return render_template('/notation')
 
+
+@app.route('/wallet')
+@login_required
+def wallet():
+    db_sess = db_session.create_session()
+    cards = db_sess.query(Cards).filter(Cards.user_id == current_user.id).all()
+    print(cards)
+    return render_template('wallet.html', your_cards=cards)
+
+
+@app.route('/edit_card/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_card(id):
+    form = AddCard()
+    if request.method == 'GET':
+        db_sess = db_session.create_session()
+        card = db_sess.query(Cards).filter(Cards.id == id, Cards.user_id == current_user.id).first()
+        if card:
+            form.number.data = card.number
+            form.expiration_date.data = card.expiration_date
+            form.cvc.data = card.cvc
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        card = db_sess.query(Cards).filter(Cards.id == id, Cards.user_id == current_user.id).first()
+        if card:
+            card.number = form.number.data
+            card.expiration_date = form.expiration_date.data
+            card.cvc = form.cvc.data
+            db_sess.commit()
+            return redirect('/wallet')
+        else:
+            abort(404)
+    return render_template('add_card.html',
+                           title='Изменить сведения о карте', form=form)
+
+
+@app.route('/delete_card/<int:id>')
+@login_required
+def delete_card(id):
+    db_sess = db_session.create_session()
+    card = db_sess.query(Cards).filter(Cards.id == id, Cards.user_id == current_user.id).first()
+    if card:
+        db_sess.delete(card)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/wallet')
+
+
+@app.route('/add_card', methods=['GET', 'POST'])
+@login_required
+def add_card():
+    form = AddCard()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            if_cards = db_sess.query(Cards).filter(Cards.number == form.number.data,
+                                                   Cards.user_id == current_user.id).all()
+            if not if_cards:
+                new_card = Cards(
+                    number=form.number.data,
+                    expiration_date=form.expiration_date.data,
+                    cvc=form.cvc.data,
+                    user_id=current_user.id
+                )
+                db_sess.add(new_card)
+                db_sess.commit()
+                return redirect('/wallet')
+            else:
+                return '''Такая карта уже добавлена'''
+    return render_template('add_card.html', form=form, title='Добавить карту')
+
+
+@app.route('/top_up_balance/<int:id>', methods=['GET', 'POST'])
+@login_required
+def top_up_balance(id):
+    form = Balance()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.id == current_user.id).first()
+            user.balance += int(form.balance.data)
+            db_sess.commit()
+            return redirect('/wallet')
+    return render_template('top_up_balance.html', title='Пополнить баланс кошелька', form=form)
+
+
+@app.route('/make_review/<int:id>', methods=['GET', 'POST'])
+@login_required
+def make_review(id):
+    id = id
+    form = MakeReview()
+    db_sess = db_session.create_session()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_review = Reviews(
+                product_id=id,
+                review=form.review.data,
+                user_id=current_user.id
+            )
+            db_sess.add(new_review)
+            db_sess.commit()
+            return redirect(url_for('reviews', id=id))
+    return render_template('make_review.html', form=form, title="Оставить отзыв")
+
+
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect("/")
